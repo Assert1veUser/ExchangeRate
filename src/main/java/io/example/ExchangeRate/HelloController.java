e2c0bec096dfbece0bb6405c2a09acbee02af84e
Assert1veUser <tony.markin2017@yandex.ru>
2024-07-15 14:00:13 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/io/example/ExchangeRate/ExchangeRateApplication.java 	new file:   src/main/java/io/example/ExchangeRate/HelloController.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/io/example/ExchangeRate/ExchangeRateApplicationTests.java

---
+++ src/main/java/io/example/ExchangeRate/HelloController.java
@@ -0,0 +1,21 @@
+package io.example.ExchangeRate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@RequestParam(value = "name", required = false) String name){
+        return new ResponseEntity<>(String.format("Hello, %s! Today is %s!", name,
+                LocalDate.now().format(DateTimeFormatter.ISO_DATE)), HttpStatus.OK);
+    }
+
+
+}
